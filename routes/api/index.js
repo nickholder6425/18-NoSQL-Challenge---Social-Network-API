0a1cca2c90c1e52224bd67a16dfbbc130fb88c58
nicholasholder <ngholder@hotmail.com>
2024-03-05 16:34:18 -0600

    modifying files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // import all API routes from /api/index.js
-const apiRoutes = require("./api");
+const apiRoutes = require("../api");
 
 router.use("/api", apiRoutes);
 
