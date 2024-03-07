c62986e9ff0424082d7bb0aaeef87d6741d9fad8
nicholasholder <ngholder@hotmail.com>
2024-03-06 22:07:33 -0600

    modified server.js

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ app.use(express.static('public'));
 // if MONGODB_URI exists, connect to that DB
 // otherwise short-circuit to local MongoDB server's DB
 // MongoDB finds and connects to DB if exists or creates if it doesn't
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network-api", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,  // Deprecated option, but may be needed for older versions of Mongoose
     useCreateIndex: true,
 });
+
 
 // log mongo queries being executed
 mongoose.set("debug", true);
